e050394341e04d0a8615752dbdf2394028a601b5
hanu329 <thanuman222@gmail.com>
2026-01-09 00:16:00 +0530

    b101

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-// Routes
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("Mongo Error:", err));
+
 app.use("/api/auth", require("./routes/auth"));
 
-// MongoDB connect
-mongoose.connect("mongodb+srv://Hanu329:Trip9452@cluster0.xn0gaae.mongodb.net/viteauth")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
-
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on " + PORT));
