d5c7364720a97b5a8b922f5283e62ce723ab669a
hanu329 <thanuman222@gmail.com>
2026-05-16 06:52:48 +0530

    img exp

--- frontend/src/assets/img.js
+++ frontend/src/assets/img.js
@@ -1,4 +1,4 @@
   
 
   export const expImg =
-  "https://res.cloudinary.com/dikkf18hu/image/upload/v1778827831/abc8_tewxug.png";
+  "https://res.cloudinary.com/dikkf18hu/image/upload/v1778893899/exp1_z6mqp1.png";
